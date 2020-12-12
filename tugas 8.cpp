6ea5ea7868e7233680184d5645375bb48385ea7c
Ferry Kurniawan <ferryku8@gmail.com>
2020-12-12 19:25:29 +0700

    TUGAS 8 C++
    Mengumpulkan tugas 8 C++

---
+++ tugas 8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int x;
+	cout<<"Inputkan Nilai X : ";
+	cin>>x;
+	switch(x){
+		case 10000 :
+			cout<<"Sepuluh ribu rupiah";
+			break;
+		case 20000 :
+			cout<<"Dua puluh ribu rupiah";
+			break;
+		case 30000 :
+			cout<<"Tiga puluh ribu rupiah";
+			break;
+		case 40000 :
+			cout<<"Empat puluh ribu rupiah";
+			break;
+		case 50000 :
+			cout<<"Lima puluh ribu rupiah";
+			break;
+		default :
+			cout<<"Tidak Terdaftar di program";
+	}
+}
